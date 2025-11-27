a682e66a74302bf845d19d8baceed7287fe65c81
Muhammad Manamil <muhammadmanamil@gmail.com>
2025-11-27 13:40:49 +0500

    ok

--- resources/views/admin/inc/navbar.blade.php
+++ resources/views/admin/inc/navbar.blade.php
@@ -24,10 +24,12 @@
                         <img src="{{ asset('assets/img/userprofile.png') }}" class="w-full h-[60px] object-cover" alt="">
                     {{-- @endif --}}
                 </div>
-                <div class="ml-3 lg:block hidden">
-                    <p class="mb-0"><strong>{{ Auth::user()->name }}</strong></p>
-                    <p class="mb-0 text-gray-500">{{ Auth::user()->email }}</p>
-                </div>
+                @if (Auth::user()->image)
+                    <div class="ml-3 lg:block hidden">
+                        <p class="mb-0"><strong>{{ Auth::user()->name }}</strong></p>
+                        <p class="mb-0 text-gray-500">{{ Auth::user()->email }}</p>
+                    </div>
+                @endif
             </div>
             <div class="">
                 <img src="{{ asset('assets/img/icons/union.png') }}" alt="">

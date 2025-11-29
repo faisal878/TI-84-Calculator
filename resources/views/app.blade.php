1869f57670c13c15e8c870381d44115b6a30d0d5
Muhammad Manamil <muhammadmanamil@gmail.com>
2025-11-29 16:30:33 +0500

    ddd

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" href="{{ asset('assets/img/favicon.ico') }}">
+    <link rel="icon" href="{{ asset('fav.jpeg') }}">
     {{ Seo()->generate() }}
     @if(@$xDefaultLink)
         <link rel="alternate" hreflang="x-default" href="{{ $xDefaultLink }}" />

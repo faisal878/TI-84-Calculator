061b4027a7137e8fb70433a75a5c823386fa57dd
Muhammad Manamil <muhammadmanamil@gmail.com>
2025-12-26 13:27:28 +0500

    web.php

--- routes/tools.php
+++ routes/tools.php
@@ -8,9 +8,9 @@ Route::get('{slug}', [ToolsController::class, 'toolsSingle'])->name('tool.single
 Route::get('{lang?}/{slug}', [ToolsController::class, 'toolsSingle'])->name('tool.single');
 
 // Route::redirect('tools/online-graphing-calculator', 'online-graphing-calculator', 301);
-Route::get('/tools/online-graphing-calculator', function () {
-    return redirect('/online-graphing-calculator', 301); // Permanent Redirect
-});
+// Route::get('/tools/online-graphing-calculator', function () {
+//     return redirect('/online-graphing-calculator', 301); // Permanent Redirect
+// });
 // Route::get('/tools/ti-30xs-calculator-online', function () {
 //     return redirect('/ti-30xs-calculator-online', 301); // Permanent Redirect
 // });
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\ContactController;
 use App\Http\Controllers\WebController;
 use Illuminate\Support\Facades\Route;
 
+Route::permanentRedirect('/tools/online-graphing-calculator', '/online-graphing-calculator');
+
 // Route::get('/', function () { return view('web.home'); })->name('home');
 Route::get('/', [WebController::class, 'home'])->name('home');
 

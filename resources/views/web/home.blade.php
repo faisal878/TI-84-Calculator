35f8b10f9c04dd6d926c65872c55e539825fd6c6
Muhammad Manamil <muhammadmanamil@gmail.com>
2025-12-26 13:36:17 +0500

    ok

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -6,14 +6,14 @@
 <div class="max-w-4xl m-auto space-y-10 p-6 ">
     <div class="content">{!! html_entity_decode(@$homeSetting->data )!!}</div>
     <div class="md:grid grid-cols-2 gap-4 justify-center my-6">
-        {{-- @if ($tools->isNotEmpty())
+        @if ($tools->isNotEmpty())
             @foreach ($tools as $item)
                 <a href="{{ url($item->slug) }}" class="p-6 md:mb-0 mb-4 rounded-lg border border-gray-300 hover:shadow-lg shadow-md cursor-pointer transition-shadow duration-300 block max-w-md">
                     <h2 class="font-bold text-xl mb-3 capitalize">{{ $item->title }}</h2>
                     <p class="text-[14px] text-[#696981] line-clamp-3">{{ $item->meta_description }}</p>
                 </a>
             @endforeach
-        @endif --}}
+        @endif
     </div>
 </div>
 

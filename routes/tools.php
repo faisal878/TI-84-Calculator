0aa6a089ad3a0abd75d1c267df76534a2c10a77e
Muhammad Manamil <muhammadmanamil@gmail.com>
2025-12-26 13:19:35 +0500

    ok

--- routes/tools.php
+++ routes/tools.php
@@ -7,16 +7,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('{slug}', [ToolsController::class, 'toolsSingle'])->name('tool.single');
 Route::get('{lang?}/{slug}', [ToolsController::class, 'toolsSingle'])->name('tool.single');
 
-Route::get('/tools/ti-30xs-calculator-online', function () {
-    return redirect('/ti-30xs-calculator-online', 301); // Permanent Redirect
-});
+Route::redirect(
+    '/tools/online-graphing-calculator',
+    '/online-graphing-calculator',
+    301
+);
 
-Route::get('/tools/online-graphing-calculator', function () {
-    return redirect('/online-graphing-calculator', 301); // Permanent Redirect
-});
+// Route::get('/tools/ti-30xs-calculator-online', function () {
+//     return redirect('/ti-30xs-calculator-online', 301); // Permanent Redirect
+// });
 
-Route::get('/tools/ti-84-calculator', function () {
-    return redirect('/ti-84-calculator', 301); // Permanent Redirect
-});
+// Route::get('/tools/online-graphing-calculator', function () {
+//     return redirect('/online-graphing-calculator', 301); // Permanent Redirect
+// });
+
+// Route::get('/tools/ti-84-calculator', function () {
+//     return redirect('/ti-84-calculator', 301); // Permanent Redirect
+// });
 
 
